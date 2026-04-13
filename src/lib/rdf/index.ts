97ce06b52cfa96f44d29cf81ba520ea9969e83ad
Daniel Heward-Mills <5824561+danielhmills@users.noreply.github.com>
2026-04-13 17:02:35 -0400

    feat: export serializeToTurtle from RDF module

--- src/lib/rdf/index.ts
+++ src/lib/rdf/index.ts
@@ -1,2 +1,2 @@
-export { serializeToRDF, escapeXml, deriveBaseUri } from './serializer';
+export { serializeToRDF, serializeToTurtle, escapeXml, deriveBaseUri } from './serializer';
 export { parseRDF, RDFParseError } from './parser';
